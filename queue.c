cdf0d6c0a3ba4439d7a38fc0dfac933047da262a
Mike <mike18913235447@gmail.com>
2025-02-25 11:28:04 +0800

    Implement q_size
    Implement this function to return the size of a queue.
    Using list_for_each to enumerate the element in the queue,
    and count will be added with each element.
    
    Change-Id: Ia2fe2bb6a2241fa5523f83a78fbc072b7fb2781d

--- queue.c
+++ queue.c
@@ -72,7 +72,13 @@ element_t *q_remove_tail(struct list_head *head, char *sp, size_t bufsize)
 /* Return number of elements in queue */
 int q_size(struct list_head *head)
 {
-    return -1;
+    if (!head)
+        return 0;
+    struct list_head *cur;
+    int size = 0;
+    list_for_each (cur, head)
+        size++;
+    return size;
 }
 
 /* Delete the middle node in queue */
