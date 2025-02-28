0eb939238f0cd67c179186a0c279f2c272bdaf8c
MikeChen <mike18913235447@gmail.com>
2025-02-28 23:17:06 +0800

    Implement q_reverse
    Implement q_reverse to reverse elements in queue.
    The function uses a pointer 'tmp' to record the next element and
    enumerate every element to reverse the prev and next until encounter
    the head.
    
    Change-Id: I8eddcbce5f485f4a562c2a6a948d077eb714be95

--- queue.c
+++ queue.c
@@ -131,7 +131,19 @@ void q_swap(struct list_head *head)
 }
 
 /* Reverse elements in queue */
-void q_reverse(struct list_head *head) {}
+void q_reverse(struct list_head *head)
+{
+    if (!head)
+        return;
+    struct list_head *cur = head;
+    struct list_head *tmp;
+    do {
+        tmp = cur->next;
+        cur->next = cur->prev;
+        cur->prev = tmp;
+        cur = tmp;
+    } while (cur != head);
+}
 
 /* Reverse the nodes of the list k at a time */
 void q_reverseK(struct list_head *head, int k)
