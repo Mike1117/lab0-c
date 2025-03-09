696fdc39327de1e660c324af45391627e9f56423
MikeChen <mike18913235447@gmail.com>
2025-03-10 01:46:26 +0800

    Add feature to remove outliers
    This is a simple implementation to remove outliers.
    Just sort and remove the certain range of head and
    tail of the sorted array.
    
    Change-Id: I407277fe86da70bac8b52e893455a1cad5deac20

--- queue.c
+++ queue.c
@@ -74,8 +74,8 @@ element_t *q_remove_head(struct list_head *head, char *sp, size_t bufsize)
         return NULL;
     element_t *element = list_first_entry(head, element_t, list);
     list_del(&element->list);
-    if (sp && bufsize) {
-        strncpy(sp, element->value, bufsize);
+    if (sp) {
+        strncpy(sp, element->value, bufsize - 1);
         sp[bufsize - 1] = '\0';
     }
     return element;
@@ -88,8 +88,8 @@ element_t *q_remove_tail(struct list_head *head, char *sp, size_t bufsize)
         return NULL;
     element_t *element = list_last_entry(head, element_t, list);
     list_del(&element->list);
-    if (sp && bufsize) {
-        strncpy(sp, element->value, bufsize);
+    if (sp) {
+        strncpy(sp, element->value, bufsize - 1);
         sp[bufsize - 1] = '\0';
     }
     return element;
